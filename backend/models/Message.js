9440416264b1f7d17826de01b03e8ab8a12db991
unknown <hai.dang@vnpd.com>
2026-01-16 15:35:33 +0700

    feat(be-models): add models

---
+++ backend/models/Message.js
@@ -0,0 +1,25 @@
+// models/Message.js
+const mongoose = require('mongoose');
+
+const messageSchema = new mongoose.Schema({
+  room: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Room',
+    required: true
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  username: {
+    type: String,
+    required: true
+  },
+  content: {
+    type: String,
+    required: true
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Message', messageSchema);
---
+++ backend/models/Room.js
@@ -0,0 +1,29 @@
+// models/Room.js
+const mongoose = require('mongoose');
+
+const roomSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    default: ''
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  ownerName: {
+    type: String,
+    required: true
+  },
+  members: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }]
+}, { timestamps: true });
+
+module.exports = mongoose.model('Room', roomSchema);
+
